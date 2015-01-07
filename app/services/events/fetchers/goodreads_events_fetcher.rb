eaaf663dfcc834f266c2f7b380b81eab26fab251
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-06 19:28:55 -0500

    Added goodreads event fetcher

--- app/services/events/fetchers/goodreads_events_fetcher.rb
+++ app/services/events/fetchers/goodreads_events_fetcher.rb
@@ -6,8 +6,28 @@ module Events
         @client = Goodreads::Client.new
       end
 
-      def fetch
-        @events = @client.shelf(3734291, 'currently-reading')
+      def events
+        @events ||= @client.shelf(3734291, 'currently-reading')[:books]
+      end
+
+      def fetch_and_save!
+        events.each do |event|
+          save event
+        end
+      end
+
+      private
+
+      def save(event)
+        new_event = GoodreadsEvent.new
+
+        new_event.uuid = event[:id]
+        new_event.happened_at = event[:started_at]
+        new_event.image_url = event[:book][:small_image_url]
+        new_event.url = event[:book][:link]
+        new_event.title = event[:book][:title]
+
+        new_event.save
       end
 
     end
