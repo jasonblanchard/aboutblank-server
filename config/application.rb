4ffe67c0d5b670e08fb6c09babe0509d71d29b2d
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-06 21:23:22 -0500

    Added CORS

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module AboutblankServer
 
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
+        origins '*'
+
         resource '*',
           :headers => :any,
           :methods => [:get, :options],
